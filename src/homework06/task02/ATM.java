3e6ccff8120503019d5f92b2d6b3c7d6fc79313a
Nikita Krivenia <nikita.krivenia@gmail.com>
2022-03-21 10:00:09 +0300

    Home work #6 fix

--- src/homework06/task02/ATM.java
+++ src/homework06/task02/ATM.java
@@ -25,6 +25,7 @@ public class ATM {
     }
 
     public boolean getCache() {
+        System.out.print("Введите сумму: ");
         Scanner scanner = new Scanner(System.in);
         try {
             int money = scanner.nextInt();
@@ -36,10 +37,29 @@ public class ATM {
             int asked50 = money % 100 / 50;
             int asked20 = money % 100 % 50 / 20;
             int askedOther = money % 100 % 50 % 20;
-            if (askedOther > 0
-                    || asked20 > den20
+            if (asked100 == 0 && asked50 == 0 && asked20 == 0){
+                System.out.println("Банкомат может выдавать только купюры номиналом 20, 50 и 100.");
+                return false;
+            }
+            while (den100 < asked100){
+                asked100--;
+                asked50 += 2;
+            }
+            while (money - asked100 * 100 < 20 && money - asked100 * 100 < 50) {
+                asked100--;
+                    asked50++ ;
+            }
+            if (asked100 * 100 + asked50 * 50 == money) {}
+            else if (money - (asked100 * 100 + asked50 * 50 + askedOther) < 20) {
+                asked50--;
+            }
+             asked20 = (money - (asked100 * 100 + asked50 * 50)) / 20;
+            if (asked100 * 100 + asked50 * 50 + asked20 * 20 == money) {
+                askedOther = 0;
+            }
+            if (asked20 > den20
                     || asked50 > den50
-                    || asked100 > den100) {
+                    || asked100 > den100 && asked50 * 2 > den100 && asked20 * 5 > den100) {
                 System.out.println("Банкомат может выдавать только купюры номиналом 20, 50 и 100.");
                 return false;
             } else {
